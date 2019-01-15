a0d3487a66e9ce3cf9279024f6efdff6b0bc6211
Dennis Roelvink <dennis.roelvink@gmail.com>
2019-01-15 23:48:23 +0100

    ree?

---
+++ script.js
@@ -0,0 +1,66 @@
+
+var textarea = document.getElementById('typed');
+var pretyping = true;
+
+var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789";
+var command = "";
+
+var options = {
+    strings: [
+        "`Dennis Roelvink [15/01/2019]\n"+
+        "(c) Dennis Roelvink. All rights reserved.\n"+
+        "MAIN>`" +
+        "^1000HELP^1000\n" +
+        "\n" +
+        "`Available commands:\n"+
+        "* INFO\n" +
+        "* ABOUT\n" + 
+        "* PROJECTS\n" + 
+        "* RESUME\n" + 
+        "* CONTACT\n" + 
+        "\n" +
+        "MAIN>`"
+    ],
+    onComplete: function(self) {
+        //textarea.disabled = false;
+        pretyping = false;
+    },
+    typeSpeed: 80
+}
+
+document.onkeyup = function(e) {
+    if(!pretyping) {
+        if(IsAllowedToType(e.key.toUpperCase())) {
+            textarea.value += e.key.toUpperCase();
+            command += e.key.toUpperCase();
+        }
+        else if (e.key === "Backspace") {
+            if(textarea.value[textarea.value.length-1] !== ">") textarea.value = textarea.value.slice(0, textarea.value.length-1);
+            
+            if(command.length > 0) command = command.slice(0, command.value.length -1);
+        }
+        else if (e.key === "Enter") {
+
+            switch(command) {
+                case "CLEAR":
+                    textarea.value += "\nClever person...\nMAIN>";
+                    break;
+
+                    default:
+                    textarea.value += "\nUnknown command '"+command.trim()+"'\nMAIN>";
+                    break;
+            }
+            command = "";
+        }
+    }
+
+    
+}
+
+function IsAllowedToType(key) {
+    "";
+    return (allowed.search(key) >= 0);
+}
+
+
+var typed = new Typed("#typed", options);
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.get('/style.css', function(req, res) {
+	res.sendFile(path.join(__dirname + '/style.css'));
+});
+
+app.get('/script.js', function(req, res) {
+	res.sendFile(path.join(__dirname + '/script.js'));
+});
+
 app.get('/*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
