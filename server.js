a2ea34567dd71b39edac5efd5684690725f87447
Dennis Roelvink <dennis.roelvink@gmail.com>
2019-01-15 23:44:21 +0100

    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + '/public'));
-//app.get('/*', function(req, res) {
-//	res.sendFile(path.join(__dirname + '/index.html'));
-//});
-
-app.get('*', function(req, res){
-  res.sendfile(__dirname + '/public/index.html');
+//app.use(express.static(__dirname + '/public'));
+app.get('/*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+//app.get('*', function(req, res){
+//  res.sendfile(__dirname + '/public/index.html');
+//});
 
 app.listen(PORT);
