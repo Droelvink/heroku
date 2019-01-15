1995e6d617184ba79965fc5598a9a9bb180c6a80
Dennis Roelvink <dennis.roelvink@gmail.com>
2019-01-15 23:42:13 +0100

    CMON DUDE

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
-	res.sendFile(path.join(__dirname + '/style.css'));
-	res.sendFile(path.join(__dirname + '/script.js'));
+app.use(express.static(__dirname + '/public'));
+//app.get('/*', function(req, res) {
+//	res.sendFile(path.join(__dirname + '/index.html'));
+//});
+
+app.get('*', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT);
